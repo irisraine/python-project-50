5403793d0c404884143e08599b7a3516be06fbc1
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-08-27 20:17:12 +0000

    third stage of project #3

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,34 @@
+import json
+
+
+def stringify(value):
+    return str(value).lower()
+
+
+def generate_diff(file_path1, file_path2):
+    try:
+        with open(file_path1) as json1:
+            json1_content = json.load(json1)
+        with open(file_path2) as json2:
+            json2_content = json.load(json2)
+    except FileNotFoundError:
+        return "File doesn't exist"
+
+    keys_all = set(json1_content.keys()) | set(json2_content.keys())
+    keys_sorted = sorted(list(keys_all))
+    diff_list = []
+    diff_list.append("{")
+    for key in keys_sorted:
+        if key in json1_content and key in json2_content:
+            if json1_content[key] == json2_content[key]:
+                diff_list.append(f'    {key}: {stringify(json1_content[key])}')
+            else:
+                diff_list.append(f'  - {key}: {stringify(json1_content[key])}')
+                diff_list.append(f'  + {key}: {stringify(json2_content[key])}')
+        elif key in json1_content:
+            diff_list.append(f'  - {key}: {stringify(json1_content[key])}')
+        elif key in json2_content:
+            diff_list.append(f'  + {key}: {stringify(json2_content[key])}')
+    diff_list.append("}")
+
+    return '\n'.join(diff_list)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -10,7 +11,7 @@ def main():
     parser.add_argument("-f", "--format", help="set format of output")
 
     args = parser.parse_args()
-
+    print(generate_diff(args.first_file, args.second_file))
 
 if __name__ == '__main__':
     main()
