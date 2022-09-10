d911d6f5da0afea034a759e3c1149ceeaa907202
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-09-10 18:11:27 +0000

    add tests

---
+++ gendiff/tests/test_generate_diff.py
@@ -0,0 +1,11 @@
+from gendiff import generate_diff
+
+
+def test_generate_diff():
+    try:
+        with open("fixtures/expected/json_plain_result.txt") as json_plain:
+            result = "\n".join(json_plain.read().splitlines())
+    except FileNotFoundError:
+        return "File doesn't exist"
+
+    assert generate_diff("fixtures/file1.json", "fixtures/file2.json") == result
