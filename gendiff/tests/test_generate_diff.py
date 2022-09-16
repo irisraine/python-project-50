fa6a804a68a36ea40b6a88ce514508e3b50847d7
Iris-Raine <iris.raine.darkest.star@gmail.com>
2022-09-16 20:50:14 +0000

    update tests

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -2,31 +2,30 @@ from gendiff import generate_diff
 from gendiff.parse import load_content
 from gendiff.format import stylish
 
-json_simple1 = load_content("fixtures/file1.json")
-json_simple2 = load_content("fixtures/file2.json")
-yaml_simple1 = load_content("fixtures/file1.yaml")
-yaml_simple2 = load_content("fixtures/file2.yaml")
-json_nested1 = load_content("fixtures/file1_nested.json")
-json_nested2 = load_content("fixtures/file2_nested.json")
-yaml_nested1 = load_content("fixtures/file1_nested.yaml")
-yaml_nested2 = load_content("fixtures/file2_nested.yaml")
 
-
-def test_simple_diff():
+def test_simple_stylish_diff():
     try:
         with open("fixtures/expected/simple_stylish.txt") as simple_stylish:
             result = "\n".join(simple_stylish.read().splitlines())
     except FileNotFoundError:
         return "File doesn't exist"
-    assert stylish(generate_diff(json_simple1, json_simple2)) == result
-    assert stylish(generate_diff(yaml_simple1, yaml_simple2)) == result
+    assert stylish(generate_diff(
+        load_content("fixtures/file1.json"),
+        load_content("fixtures/file2.json"))) == result
+    assert stylish(generate_diff(
+        load_content("fixtures/file1.yaml"),
+        load_content("fixtures/file2.yaml"))) == result
 
 
-def test_nested_diff():
+def test_nested_stylish_diff():
     try:
         with open("fixtures/expected/nested_stylish.txt") as nested_stylish:
             result = "\n".join(nested_stylish.read().splitlines())
     except FileNotFoundError:
         return "File doesn't exist"
-    assert stylish(generate_diff(json_nested1, json_nested2)) == result
-    assert stylish(generate_diff(yaml_nested1, yaml_nested2)) == result
+    assert stylish(generate_diff(
+        load_content("fixtures/file1_nested.json"),
+        load_content("fixtures/file2_nested.json"))) == result
+    assert stylish(generate_diff(
+        load_content("fixtures/file1_nested.yaml"),
+        load_content("fixtures/file2_nested.yaml"))) == result
